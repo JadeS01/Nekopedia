a90b4bd8bbe28a41d9b4dc5d89318f52fa64287c
Jade Simien <baba3of4@gmail.com>
2023-03-24 20:56:13 -0700

    pagination

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -4,8 +4,13 @@
 	import { onMount } from "svelte";
 	import type { IData } from "../lib/utils/IData";
 	import { writable } from "svelte/store";
+	import Button from "$lib/components/Button.svelte";
 
 	let data: IData[];
+	const pageSize = 6;
+	let totalPages = 1;
+	const currPage = writable<number>(1);
+	let displayItems: IData[];
 	const modalState = writable(false);
 	const emptyData = {
 		CatId: "",
@@ -30,10 +35,37 @@
 		modalData.set(emptyData);
 	}
 
+	function prevPage() {
+		if ($currPage > 1) {
+			let page = $currPage - 1;
+			currPage.set(page);
+			displayItems = data.slice(
+				($currPage - 1) * pageSize,
+				$currPage * pageSize
+			);
+		}
+	}
+
+	function nextPage() {
+		if ($currPage < totalPages) {
+			let page = $currPage + 1;
+			currPage.set(page);
+			displayItems = data.slice(
+				($currPage - 1) * pageSize,
+				$currPage * pageSize
+			);
+		}
+	}
+
 	onMount(async () => {
 		try {
 			const res = await fetch("https://api.neko-atsume.emshea.com/cats");
 			data = await res.json();
+			totalPages = Math.ceil(data.length / pageSize);
+			displayItems = data.slice(
+				($currPage - 1) * pageSize,
+				$currPage * pageSize
+			);
 		} catch (error) {
 			console.error(error);
 		}
@@ -48,15 +80,23 @@
 <div
 	class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 font-sans font-normal text-base"
 >
-	{#if data}
+	{#if displayItems}
 		{#if $modalState}
 			<Modal handleState={closeModal} {modalData} />
 		{/if}
-		{#each data as cat}
+		{#each displayItems as cat}
 			<div class="flex-1">
 				<Card {cat} {openModal} />
 			</div>
 		{/each}
+		<div class="grid grid-cols-2 sm:grid-cols-2">
+			<div on:click={prevPage}>
+				<Button text="Back" />
+			</div>
+			<div on:click={nextPage}>
+				<Button text="Next" />
+			</div>
+		</div>
 	{:else}
 		<div>nothing</div>
 	{/if}
